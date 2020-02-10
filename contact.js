ee9d40878b47f7d76fe6721938122e036fab2593
jirayus-kard <58967004+jirayus-kard@users.noreply.github.com>
2020-02-11 02:07:05 +0700

    11/02

---
+++ contact.js
@@ -0,0 +1,9 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDNbvDSfNbdxMetyfn7WJ7Vhl8U9-S0GgE",
+    authDomain: "contact-lab5.firebaseapp.com",
+    projectId: "contact-lab5",
+};
+
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
